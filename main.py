19a929c7add71a98722ba1f88ef4cf8fbcee3e03
Diamond Tusks <90947391+DiamondTusks@users.noreply.github.com>
2022-09-14 00:38:10 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
 # print(soup.title)
-article_tag = soup.find(name="a", class_="titlelink")
-article_text = article_tag.getText()
-article_link = article_tag.get("href")
-article_upvote = soup.find(name="span", class_="score").getText()
+articles = soup.find_all(name="a", class_="titlelink")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
 
-print(article_text)
-print(article_link)
-print(article_upvote)
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
 
 
 
